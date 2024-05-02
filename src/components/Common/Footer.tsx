d551501800313e7b5a9333bf2eb06c8a91328b70
Kimani-Dominic <muiruridominic001@gmail.com>
2024-05-02 17:24:10 +0300

    App Layout

---
+++ src/components/Common/Footer.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Common.css';
+
+const Footer: React.FC = () => {
+  return (
+    <div className='footer'>
+        Footer
+    </div>
+  );
+};
+
+export default Footer;
---
+++ src/components/Common/Loader.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import "./Common.css";
+
+const Loader: React.FC = () => {
+  return (
+    <div><div className="loader"></div></div>
+  );
+};
+
+export default Loader;
