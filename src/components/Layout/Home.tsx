cacd22fbc5754558c34698323cebf8b0aa2de8ed
Kimani-Dominic <muiruridominic001@gmail.com>
2024-05-02 17:23:28 +0300

    App Layout

---
+++ src/components/Layout/Home.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Carasol from '../Pages/Carasol';
+
+const Home: React.FC = () => {
+  return (
+    <div>
+      <Carasol />
+    </div>
+  );
+};
+
+export default Home;
